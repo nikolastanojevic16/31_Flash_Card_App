7d6dd5feff705086519f1b0024b2f47c7283ca6c
nikolastanojevic16 <nikolastanojevic16@gmail.com>
2024-04-14 15:33:26 +0200

    added button functionality

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+# ------------------ DATA ------------------ #
+
+data = pandas.read_csv("data/french_words.csv")
+data_list_of_dict = data.to_dict(orient="records")
+card = {}
+
+
+def flip_card():
+    canvas.itemconfig(canvas_image, image=card_back_image)
+    canvas.itemconfig(title_text, fill="white", text=list(card.keys())[1])
+    canvas.itemconfig(word_text, fill="white", text=card["English"])
+
+
+def next_card():
+    global card, flip_timer
+    window.after_cancel(flip_timer)
+    card = random.choice(data_list_of_dict)
+    canvas.itemconfig(canvas_image, image=card_front_image)
+    canvas.itemconfig(title_text, text=list(card.keys())[0], fill="black")
+    canvas.itemconfig(word_text, text=card["French"], fill="black")
+    flip_timer = window.after(3000, flip_card)
+
 
 # ------------------ UI SETUP --------------------- #
 
@@ -8,19 +33,24 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, flip_card)
+
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_image)
+card_back_image = PhotoImage(file="images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image=card_front_image)
 title_text = canvas.create_text(400, 150, text="Title", fill="black", font=("Ariel", 40, "italic"))
 word_text = canvas.create_text(400, 263, text="Word", fill="black", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 right_button_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_button_image, highlightthickness=0)
+right_button = Button(image=right_button_image, highlightthickness=0, command=next_card)
 right_button.grid(column=1, row=1)
 
 wrong_button_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_button_image, highlightthickness=0)
+wrong_button = Button(image=wrong_button_image, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
+
+next_card()
 
 window.mainloop()
